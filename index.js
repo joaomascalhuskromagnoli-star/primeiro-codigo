586df40217150fa716082492fcce5845fc224849
joaomascalhuskromagnoli-star <joao.mascalhusk.romagnoli@escola.pr.gov.br>
2025-09-18 18:14:04 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 document.getElementById("texto").innerHTML="Meu primeiro texto <b>JS</b>!";
 console.log("Oi isso é um um console.log");
+
+let a,b,c;
+a=5;
+b=6;
+c=7;
+
+let pessoa = "Dimitri";
+
+function minhafunção (){
+    document.write(5+5);
+    document.write(5+5);
+    document.write(5+5);
+    document.write(5+5);
+}
